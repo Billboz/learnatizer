8d142ff3b8233a4980a6fc8d2d418339b72c6f1f
billboz1@gmail.com <billboz1@gmail.com>
2014-12-01 14:45:28 -0500

    Got the Flash Messages Working
    As well as cleaned up some code in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,19 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  password   :string(255)
-#  role       :string(255)
-#  email      :string(255)
-#  blog       :string(255)
-#  twitter    :string(255)
-#  facebook   :string(255)
-#  linkedin   :string(255)
-#  points     :integer
-#  created_at :datetime
-#  updated_at :datetime
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  password        :string(255)
+#  role            :string(255)
+#  email           :string(255)
+#  blog            :string(255)
+#  twitter         :string(255)
+#  facebook        :string(255)
+#  linkedin        :string(255)
+#  points          :integer
+#  created_at      :datetime
+#  updated_at      :datetime
+#  password_digest :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -21,21 +22,21 @@ class User < ActiveRecord::Base
 #  has_many :reservations
 #  belongs_to :campus
 
-#  has_secure_password
+  has_secure_password
 
 #  validates :name, :role, :campus_id, presence: true
-#  validates :password, length: { minimum: 6}
+  validates :password, length: { minimum: 6}
 
 
-#  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-#  validates :email, presence: true,
-#            format: { with: VALID_EMAIL_REGEX},
-#            uniqueness:  { case_sensitive: false}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            format: { with: VALID_EMAIL_REGEX},
+            uniqueness:  { case_sensitive: false}
 
-#  validates :password, length: { minimum: 6 }
-#  def admin?
-#    self.role == 'staff'
-#  end
+  validates :password, length: { minimum: 6 }
+  def admin?
+    self.role == 'staff'
+  end
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126192140) do
+ActiveRecord::Schema.define(version: 20141201172754) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20141126192140) do
     t.integer  "points"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
