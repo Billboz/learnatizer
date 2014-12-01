0d5d7898a9cc6489d412c93a9d27d8c9b5f941c7
billboz1@gmail.com <billboz1@gmail.com>
2014-11-30 20:23:43 -0500

    Got the signup form up

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,25 @@
 #
 
 class User < ActiveRecord::Base
+
+#  has_many :reservations
+#  belongs_to :campus
+
+#  has_secure_password
+
+#  validates :name, :role, :campus_id, presence: true
+#  validates :password, length: { minimum: 6}
+
+
+#  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#  validates :email, presence: true,
+#            format: { with: VALID_EMAIL_REGEX},
+#            uniqueness:  { case_sensitive: false}
+
+#  validates :password, length: { minimum: 6 }
+#  def admin?
+#    self.role == 'staff'
+#  end
+
+
 end
